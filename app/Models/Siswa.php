7cdda5ec68e893d9c27166e6dd34f0de801c0d75
nabilramdan17 <crfganteng17@gmail.com>
2026-07-30 12:14:27 +0700

    first commit

---
+++ app/Models/Siswa.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+// app/Models/Siswa.php
+class Siswa extends Model
+{
+ use HasFactory;
+ protected $fillable = [
+ 'nis', 'nama', 'kelas', 'tanggal_mulai_pkl',
+ 'tanggal_selesai_pkl', 'perusahaan_id',
+ ];
+ public function perusahaan()
+{
+ return $this->belongsTo(Perusahaan::class);
+}
+}
+
