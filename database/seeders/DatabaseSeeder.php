c57e15db62e890725a913fed9b1b71c203f1aa68
nabilramdan17 <crfganteng17@gmail.com>
2026-07-31 10:46:14 +0700

    update tugas lagi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PerusahaanSeeder::class,
             KompetensiSeeder::class,
+            Siswaseeder::class,
         ]);
     }
 }
